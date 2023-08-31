4022c98f1c8e27b173d7986fd1ab0b8b2c2cb537
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-31 12:06:42 +0100

    feat: useEffect  almost complete.

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,27 @@
-// make an api call using useEffect
+// make an api call using
+import { useEffect, useState } from "react";
 
 export const APICall = () => {
+  // Your code here!
+  const [data, setData] = useState([]);
 
-    // Your code here!
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchToDO = async () => {
+      const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+      const result = await data.json();
+      return setData(result);
+    };
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+    // call the function
+    fetchToDO()
+      // make sure to catch any error
+      .catch(console.error);
+  }, []);
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>Data: {data}</p>
+    </>
+  );
+};
